c9224a1bd84efce2a319571159dc7165f0e3e052
RatBoyJim <>
2021-03-11 10:48:35 +0000

    tidied data loader

--- src/main/java/com/pedaledal/Pedaledal/components/DataLoader.java
+++ src/main/java/com/pedaledal/Pedaledal/components/DataLoader.java
@@ -57,58 +57,71 @@ public class DataLoader implements ApplicationRunner {
         Manufacturer boss = new Manufacturer("Boss", "The best pedal cunts", "www.boss-pedals.com");
         manufacturerRepository.save(boss);
 
-//        ArrayList<Manufacturer> blooperMans = new ArrayList<>();
-//        blooperMans.add(ChaseBlissAudio);
+        Manufacturer strymon = new Manufacturer("Strymon", "Expensive", "Webshite");
+        manufacturerRepository.save(strymon);
 
-//        new ArrayList<Manufacturer>(){{add(ChaseBlissAudio);}}
 
 //        PEDALS
-        Pedal pedal1 = new Pedal("Blooper", "Fancy", "One of my favourites", true, PedalCategory.OTHER, EffectType.LOOPER, 5, "", 49900, 33000, 122, 67, 40, true, false, "Top", true);
-        pedalRepository.save(pedal1);
-        pedal1.addManufacturer(ChaseBlissAudio);
-        pedalRepository.save(pedal1);
+        Pedal blooper = new Pedal("Blooper", "Fancy", "One of my favourites", true, PedalCategory.OTHER, EffectType.LOOPER, 5, "", 49900, 33000, 122, 67, 40, true, false, "Top", true);
+        pedalRepository.save(blooper);
+        ChaseBlissAudio.addPedal(blooper);
+        manufacturerRepository.save(ChaseBlissAudio);
 
-        Pedal pedal2 = new Pedal("DS-1", "something", "industry standard", false, PedalCategory.SATURATION, EffectType.DISTORTION, 5, "", 49900, 33000, 122, 67, 40, true, false, "Top", true);
-        pedalRepository.save(pedal2);
-        pedal2.addManufacturer(boss);
-        pedalRepository.save(pedal2);
+        Pedal mt2w = new Pedal("MT-2w", "Metal Zone", "OOOFT!", false, PedalCategory.SATURATION, EffectType.DISTORTION, 4, "", 11900, 5000, 122, 67, 40, false, false, "Side", false);
+        pedalRepository.save(mt2w);
+        boss.addPedal(mt2w);
+        manufacturerRepository.save(boss);
 
-        Pedal pedal3 = new Pedal("MT-2w", "Metal Zone", "OOOFT!", false, PedalCategory.SATURATION, EffectType.DISTORTION, 4, "", 11900, 5000, 122, 67, 40, false, false, "Side", false);
-        pedalRepository.save(pedal3);
-        pedal3.addManufacturer(boss);
-        pedalRepository.save(pedal3);
+        Pedal ds1 = new Pedal("DS-1", "something", "industry standard", false, PedalCategory.SATURATION, EffectType.DISTORTION, 5, "", 49900, 33000, 122, 67, 40, true, false, "Top", true);
+        pedalRepository.save(ds1);
+        ds1.setManufacturers(boss);
+        pedalRepository.save(ds1);
 
-        Pedal pedal4 = new Pedal("Deco", "Cool cool cool", "Love it", false, PedalCategory.SATURATION, EffectType.OVERDRIVE, 4, "", 27900, 18500, 115, 102, 45, false, true, "top", true);
-        pedalRepository.save(pedal4);
-        Manufacturer manufacturer1 = new Manufacturer("Strymon", "Expensive", "Webshite");
-        manufacturerRepository.save(manufacturer1);
-        manufacturer1.addPedal(pedal4);
-        manufacturerRepository.save(manufacturer1);
-
-//        blooper.setManufacturers(new ArrayList<Manufacturer>(){{add(ChaseBlissAudio);}});
-//        pedalRepository.save(blooper);
+        Pedal deco = new Pedal("Deco", "Cool cool cool", "Love it", false, PedalCategory.SATURATION, EffectType.OVERDRIVE, 4, "", 27900, 18500, 115, 102, 45, false, true, "top", true);
+        pedalRepository.save(deco);
+        strymon.addPedal(deco);
+        manufacturerRepository.save(strymon);
 
 
 //        BOARDS
         Board board1 = new Board("Andys board", 800, 500, andy);
         boardRepository.save(board1);
-        board1.addBoardPedal(pedal4);
+        board1.addBoardPedal(deco);
         boardRepository.save(board1);
 
         Board board2 = new Board("Kennys board", 600, 350, kenny);
         boardRepository.save(board2);
 
 //        USER PEDALS ADD
-        andy.addPedal(pedal2);
+//        andy.addPedal(ds1);
+//        ds1.setUser(andy);
+//        userRepository.save(andy);
+//        pedalRepository.save(ds1);
+
+        andy.addPedal(deco);
+        deco.setUser(andy);
         userRepository.save(andy);
+        pedalRepository.save(deco);
 
+        kenny.addPedal(mt2w);
+        mt2w.setUser(kenny);
+        userRepository.save(kenny);
+        pedalRepository.save(mt2w);
 
-
-
-
-
+        kenny.addPedal(blooper);
+        blooper.setUser(kenny);
+        userRepository.save(kenny);
+        pedalRepository.save(blooper);
+//        ds1.setBoard(board1);
+//        pedalRepository.save(ds1);
 
 
     }
+
+    //        ArrayList<Manufacturer> blooperMans = new ArrayList<>();
+//        blooperMans.add(ChaseBlissAudio);
+//        new ArrayList<Manufacturer>(){{add(ChaseBlissAudio);}}
+    //        blooper.setManufacturers(new ArrayList<Manufacturer>(){{add(ChaseBlissAudio);}});
+//        pedalRepository.save(blooper);
 
 }
